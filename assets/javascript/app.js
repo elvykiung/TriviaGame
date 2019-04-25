596b2a73179bd67b83eb5a8415f16e901c27617c
Elvy Kiung <kiungh22@gmail.com>
2019-04-25 00:31:12 -0700

    create question logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,13 @@
-//set timing
-//timer for 60s
-var number = 61;
+var number;
 var intervalId;
-setTimeout(decrement, 1000 * 60);
+var correctPoint = 0;
+var incorrectPoint = 0;
+var skipQuestionPoint = 0;
+var currentQuestionNumber = 0;
+
 //  The decrement function.
 function run() {
+  number = 10;
   clearInterval(intervalId);
   intervalId = setInterval(decrement, 1000);
 }
@@ -13,34 +16,93 @@ function decrement() {
   number--;
   //  Show the number in the #show-number tag.
   $('#show-number').text(number);
-  console.log('times');
+
   //  Once number hits zero...
   if (number === 0) {
-    //  ...run the stop function.
     stop();
+    setTimeout(nextQuestion, 1000 * 2);
+    $('.btn-group-vertical').empty();
+    $('#currQuestion').html('<h3>TIME OUT!!</h3><img src= "https://media.giphy.com/media/xTiTnCfxyp6uyd8mwo/giphy.gif">');
 
-    //  Alert the user that time is up.
-    alert('Time Up!');
+    currentQuestionNumber++;
+    skipQuestionPoint++;
   }
 }
-//run();
+function reset() {
+  number;
+  intervalId;
+  correctPoint = 0;
+  incorrectPoint = 0;
+  skipQuestionPoint = 0;
+  currentQuestionNumber = 0;
+  nextQuestion();
+  setTimeout(endGame, 1000 * 15);
+}
+//set timing for over all game
+function endGame() {
+  var unansewerPoint = questions.length - currentQuestionNumber + skipQuestionPoint;
+  $('.btn-group-vertical').empty();
+  stop();
+  //when time out, show the number of correct guesses, wrong guesses, unansewer and show start over button
+  $('#currQuestion').html('You have ' + correctPoint + ' correct scorrect point! <br> You have ' + incorrectPoint + ' incorrect scorrect point! <br> You have ' + unansewerPoint + ' unansewer!<br>');
+  // Start over, do not reload the page, just reset the game
+  var restbtn = $('<button>');
+  restbtn.text('Reset');
+  restbtn.attr('class', 'btn btn-secondary btn-block mt-4');
+  $('#currQuestion').append(restbtn);
+  $('.btn').on('click', reset);
+}
+
+setTimeout(endGame, 1000 * 180);
+
+//stop function
+function stop() {
+  clearInterval(intervalId);
+}
 
 //-----------------------------------------------------
-//set questions array object
-var questions = [
-  {
-    Question: 'In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?',
-    Choices: ['A. William and Elizabeth', 'B. Joseph and Catherine', 'C. John and Mary', 'D. George and Anne'],
-    Answer: 'C. John and Mary'
+
+// next question function
+function nextQuestion() {
+  if (currentQuestionNumber >= questions.length) {
+    endGame();
+  } else {
+    run();
+
+    $('#currQuestion').text(questions[currentQuestionNumber].Question);
+    $('.btn-group-vertical').empty();
+    for (var ii = 0; ii < questions[currentQuestionNumber].Choices.length; ii++) {
+      var newButton = $('<button>');
+      newButton.text(questions[currentQuestionNumber].Choices[ii]);
+      $('.btn-group-vertical').append(newButton);
+      newButton.attr('class', 'btn btn-secondary m-1');
+      newButton.attr('type', 'button');
+    }
+
+    // track user click input on each question
+    $('.btn').on('click', function() {
+      if ($(this).text() == questions[currentQuestionNumber].Answer) {
+        // if input click is corret, then go to next questio, log the correct answer point
+        stop();
+        setTimeout(nextQuestion, 1000 * 1.5);
+        $('.btn-group-vertical').empty();
+        $('#currQuestion').html('<h3>Correct!!</h3><img src= "https://www.merriam-webster.com/assets/mw/images/article/art-wap-article-main/alright-thumbs-up-1175@1x.jpg">');
+        correctPoint++;
+        currentQuestionNumber++;
+      } else {
+        //if wrong, show the right answer, go to next question, log the incorrect answer point
+
+        stop();
+        setTimeout(nextQuestion, 1000 * 3);
+        $('.btn-group-vertical').empty();
+        $('#currQuestion').html(
+          '<h3>WRONG!!</h3><img src= "http://assets-production.rovio.com/s3fs-public/styles/product_grid_image/public/Game%20icons/dream_blast_icon.jpg?mFeeCqScpestyDg1fptUqm_.njCpANR1&itok=Zc_WgsGa"><br>'
+        );
+        $('#currQuestion').append('Correct Answer is ' + questions[currentQuestionNumber].Answer);
+        incorrectPoint++;
+        currentQuestionNumber++;
+      }
+    });
   }
-];
-
-// track user click input on each question
-
-// if input click is corret, then go to next questio, log the correct answer point
-
-//if wrong, show the right answer, go to next question
-
-//when time out, show the number of correct guesses, wrong guesses, unansewer and show start over button
-
-// Start over, do not reload the page, just reset the game
+}
+nextQuestion();
---
+++ assets/javascript/triviaQnA.js
@@ -0,0 +1,121 @@
+//set questions array object
+var questions = [
+  {
+    Question: 'In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?',
+    Choices: ['A. William and Elizabeth', 'B. Joseph and Catherine', 'C. John and Mary', 'D. George and Anne'],
+    Answer: 'C. John and Mary'
+  },
+  {
+    Question: 'When did the Liberty Bell get its name?',
+    Choices: ['A. when it was made, in 1701', 'B. when it rang on July 4, 1776', 'C. in the 19th century, when it became a symbol of the abolition of slavery', 'D. none of the above'],
+    Answer: 'C. in the 19th century, when it became a symbol of the abolition of slavery'
+  },
+
+  {
+    Question: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
+    Choices: ['A. Buttermilk', 'B. Daisy', 'C. Scout', 'D. Tulip'],
+    Answer: 'A. Buttermilk'
+  },
+
+  {
+    Question: 'The Daniel Boon museum at the home where he died can best be described how?',
+    Choices: ['A. a log cabin in Kentucky', 'B. a two-story clapboard house in Tennessee', 'C. a four-story Georgian-style home in Missouri', 'D. a three story brick house in Arkansas'],
+    Answer: 'C. a four-story Georgian-style home in Missouri'
+  },
+
+  {
+    Question: 'Which of the following items was owned by the fewest U.S. homes in 1990?',
+    Choices: ['A. home computer', 'B. compact disk player', 'C. cordless phone', 'D. dishwasher'],
+    Answer: 'B. compact disk player'
+  },
+
+  {
+    Question: 'Who holds the record for the most victories in a row on the professional golf tour?',
+    Choices: ['A. Jack Nicklaus', 'B. Arnold Palmer', 'C. Byron Nelson', 'D. Ben Hogan'],
+    Answer: 'C. Byron Nelson'
+  },
+
+  {
+    Question: 'Who is third behind Hank Aaron and Babe Ruth in major league career home runs?',
+    Choices: ['A. Reggie Jackson', 'B. Harmon Killebrew', 'C. Willie Mays', 'D. Frank Robinson'],
+    Answer: 'C. Willie Mays'
+  },
+
+  {
+    Question: 'In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?',
+    Choices: ['A. 8', 'B. 18', 'C. 38', 'D. 58'],
+    Answer: 'B. 18'
+  },
+
+  {
+    Question: 'During the 1980s for six consecutive years what breed of dog was the most popular in the U.S.?',
+    Choices: ['A. cocker spaniel', 'B. German shepherd', 'C. Labrador retriever', 'D. poodle'],
+    Answer: 'A. cocker spaniel'
+  },
+
+  {
+    Question: 'In 1985, five percent of U.S. households had telephone answering machines. By 1990 what percentage of homes had answering machines?',
+    Choices: ['A. 10 percent', 'B. 15 percent', 'C. 31 percent', 'D. 51 percent'],
+    Answer: 'C. 31 percent'
+  },
+
+  {
+    Question: 'The first black American pictured on a U.S. postage stamp was who?',
+    Choices: ['A. Frederick Douglass', 'B. Booker T. Washington', 'C. Louis Armstrong', 'D. Joe Louis'],
+    Answer: 'D. Joe Louis'
+  },
+
+  {
+    Question: 'What did the "D" in "D-Day" stand for?',
+    Choices: ['A. doom', 'B. day', 'C. Dwight (Eisenhower)', 'D. Dunkirk'],
+    Answer: 'B. day'
+  },
+
+  {
+    Question: 'The Brownie Box Camera introduced by Eastman Kodak in 1900 had a retail price of what?',
+    Choices: ['A. $1', 'B. $5', 'C. $10', 'D. $20'],
+    Answer: 'A. $1'
+  },
+
+  {
+    Question: 'Which of these characters turned 40 years old in 1990?',
+    Choices: ['A. Charlie Brown', 'B. Bugs Bunny', 'C. Mickey Mouse', 'D. Fred Flintstone'],
+    Answer: 'A. Charlie Brown'
+  },
+
+  {
+    Question: 'The Philadelphia mint started putting a "P" mint mark on quarters when?',
+    Choices: ['A. 1960', 'B. 1980', 'C. never'],
+    Answer: 'B. 1980'
+  },
+
+  {
+    Question: "Before becoming George Bush's Secretary of Defense, what was Dick Cheney's position?",
+    Choices: ['A. congressman from Wyoming', 'B. governor of New Hampshire', 'C. secretary of defense under Ronald Reagan'],
+    Answer: 'A. congressman from Wyoming'
+  },
+
+  {
+    Question: 'When Mt. St. Helens erupted on May 18, 1980, how many people were killed?',
+    Choices: ['A. 1', 'B. 57', 'C. 571'],
+    Answer: 'B. 57'
+  },
+
+  {
+    Question: 'In J. Edgar Hoover, what did the J stand for?',
+    Choices: ['A. James', 'B. John', 'C. Joseph'],
+    Answer: 'B. John'
+  },
+
+  {
+    Question: 'Florence Nightingale became known as "the Lady With the Lamp" during which war?',
+    Choices: ['A. American Civil War', 'B. Crimean War', 'C. World War I'],
+    Answer: 'B. Crimean War'
+  },
+
+  {
+    Question: 'What year was it that the Census Bureau first reported that a majority of new mothers  were remaining in the new job market?',
+    Choices: ['A. 1968', 'B. 1978', 'C. 1988'],
+    Answer: 'C. 1988'
+  }
+];
